f70dbce0bb04ca62a2fb2df4b43c750742036a00
MrWood-Dev <156034230+MrWood-Dev@users.noreply.github.com>
2025-03-19 11:35:15 +0000

    tema 3 - movimento bispo

--- xadrez_bispo.c
+++ xadrez_bispo.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+void moveBispoCimaDireita(int casas) { // função recurssiva que move bispo para cima e para a direita
+    while(casas<=5) {// laço mais externo para movimento vertical
+        printf("%i p/ Cima e\n",casas);
+            do { // laço mais interno para movimento horizontal
+                printf("%i p/ Direita e\n",casas);
+            } while (casas < 1);
+        casas++;
+    }
+    
+     // move uma casa para cima
+}
+
+
 int main() {
 
-int b = 1; // variável com a função contadora do loop 'do while'
-    do { // inicio do laço para o movimento do bispo 
-        printf("%i p/ Cima e\n",b); // move uma casa para cima
-        printf("  Direita\n"); // move uma casa para a direita
-        b++;
-    } while (b < 6);
+int b = 1; // variável com a função contadora enviada para a função recurssiva
     
-
+    moveBispoCimaDireita(b);
+    
 return 0;
 
 }   // fim do programa
