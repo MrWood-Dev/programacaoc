0e9af5b57fa961475bd238f14f4ee732cfd8571b
MrWood-Dev <156034230+MrWood-Dev@users.noreply.github.com>
2025-03-19 11:32:55 +0000

    tema 3 - movimento torre

--- xadrez_torre.c
+++ xadrez_torre.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+void moveTorreDireita(int casas) { // função recurssiva para movimento da torre
+    while (casas<=5) {  //inicio do laço para o movimento da torre
+       printf("%i-Direita\n", casas);
+        casas++;
+    }
+}
+
+
 int main() {
 
-int t = 1; // variável com função contadora do loop while
-    while (t<=5) {  //inicio do laço para o movimento da torre
-        printf("%i-Direita\n", t);
-        t++;
-    }
-
-
-return 0; 
+int t = 1; // variável com função contadora enviada para a função recurssiva
+   
+        moveTorreDireita(t);
+        
+    return 0; 
 
 }   // fim do programa
