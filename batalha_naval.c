67d7c5a113b4de1c64d9f8a842665bb0712985e7
MrWood-Dev <156034230+MrWood-Dev@users.noreply.github.com>
2025-03-20 02:54:18 +0000

    tema4 - nivel intermediario incompleto

--- batalha_naval.c
+++ batalha_naval.c
@@ -3,18 +3,46 @@
 int main() {
 
 int tabuleiro[10][10];
+int navioh[3]={3,4,5};
+int naviov[3]={5,6,7};
+int naviodl1[3]={9,8,7};
+int naviodc1[3]={1,2,3};
+int naviodl2[3]={7,6,5};
+int naviodc2[3]={5,4,3};
 
-    for(int l=0;l<=9;l++) {
+    for(int l=0;l<10;l++) {
         for(int c=0;c<=9;c++){
             tabuleiro[l][c] = 0;
-            if(l==3 && c==3 || l==3 && c==4 || l==3 && c==5) {
-                tabuleiro[l][c] = 3;
-            } 
-            if (l==5 && c==7 || l==6 && c==7 || l==7 && c==7) {
-                tabuleiro[l][c] = 3;
-            }
-            printf("%i\t",tabuleiro[l][c]);
         }
-        printf("\n\n");
     }
+
+
+        for (int i = 0; i <=2; i++)
+        {
+            tabuleiro[3][navioh[i]]=3;
+        }
+        
+        for (int i = 0; i <=2; i++)
+        {
+            tabuleiro[naviov[i]][7]=3;
+        }
+
+        for (int i = 0; i <=2; i++){
+            tabuleiro[naviodl1[i]][naviodc1[i]]=3;
+        }
+
+        for (int i = 0; i <=2; i++){
+            tabuleiro[naviodl2[i]][naviodc2[i]]=3;
+        }
+
+        
+        for (int l = 0;l<10;l++){
+            for(int c=0;c<10;c++){
+                printf("%i\t", tabuleiro[l][c]);
+            }
+            printf("\n\n");
+        }
+       
+
+        
 }
